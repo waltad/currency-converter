183a8206017dfb0241703ac72f0faf99fb9f44ae
Tadek Waluga <tadek.waluga@gmail.com>
2023-02-19 13:47:26 +0100

    new version - 2.0, allows you to select a currency

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,38 @@
-let plnValueElement = document.querySelector(".js-plnValue");
-let exchangeRateElement = document.querySelector(".js-exchangeRate");
-let resultElement = document.querySelector(".js-result");
-let formElement = document.querySelector(".js-form");
-let plnValueEnteredElement = document.querySelector(".js-plnValueEntered")
+const plnAmountElement = document.querySelector(".js-plnAmount");
+const currencyElement = document.querySelector(".js-currency");
+const resultElement = document.querySelector(".js-result");
+const formElement = document.querySelector(".js-form");
+const plnAmountEnteredElement = document.querySelector(".js-plnAmountEntered");
+const currencyEnteredElement = document.querySelector(".js-currencyEntered");
+
+const rateEUR = 4.77;
+const rateUSD = 4.45;
+const rateGBP = 5.36;
 
 formElement.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    let plnValue = plnValueElement.value;
-    let exchangeRate = exchangeRateElement.value;
+    const plnAmout = +plnAmountElement.value;
+    const currency = currencyElement.value;
 
-    let result = plnValue / exchangeRate;
+    let result;
+
+    switch (currency) {
+        case "EUR":
+            result = plnAmout / rateEUR;
+            break;
+            
+        case "USD":
+            result = plnAmout / rateUSD;
+            break;
+
+        case "GBP":
+            result = plnAmout / rateGBP;
+            break;
+
+    }
 
     resultElement.innerText = result.toFixed(2);
-    plnValueEnteredElement.innerText = plnValue;
+    plnAmountEnteredElement.innerText = plnAmountElement.value;
+    currencyEnteredElement.innerText = currencyElement.value;
 });
